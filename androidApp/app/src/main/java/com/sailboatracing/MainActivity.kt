34f8f0787ef154d50b2e56cfb30eb61dbe0c67bf
Tom N <tomngu96@gmail.com>
2026-05-12 23:43:47 -0700

    keep screen awake if app is open

--- androidApp/app/src/main/java/com/sailboatracing/MainActivity.kt
+++ androidApp/app/src/main/java/com/sailboatracing/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sailboatracing
 import android.Manifest
 import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
@@ -25,6 +26,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         requestRequiredPermissions()
 
         setContent {
